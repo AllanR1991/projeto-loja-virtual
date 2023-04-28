2bd807c899f7aa995ea474cb423ac2d6f4886f8a
Evelyn <evelyn.ribeiro0807@gmail.com>
2023-04-28 11:02:53 -0300

    classDebito

--- NucleoPagamento/Program.cs
+++ NucleoPagamento/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// using NucleoPagamento.classes;
+// ClassDebito novoDebito = new ClassDebito ();
