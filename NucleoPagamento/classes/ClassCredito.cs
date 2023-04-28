9596130043e7af76969c7f15376d23ab9e4a4bbf
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-04-28 16:06:13 -0300

    Realizado a ClassCredito com a propriedade private e utilizando metodo do Cartao

--- NucleoPagamento/classes/ClassCredito.cs
+++ NucleoPagamento/classes/ClassCredito.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace NucleoPagamento.classes
 {
-    public class ClassCredito
+    public class ClassCredito : ClassCartao;
     {
         static string PerguntaString(string pergunta)
         {
@@ -41,6 +36,50 @@ namespace NucleoPagamento.classes
             Console.Write(texto);
         }
 
-        
+        /*Atributo*/
+        private float Limite=6000;
+
+        public float valor = 200;
+        /*Acessando Metodo da classe abstrata*/
+        public void Pagar(){
+            
+            int parcelas;
+            int minParcelas = 1;
+            int maxParcelas = 12;
+
+
+            if(valor>Limite){
+                ExibeMensagemPulandoLinha("O Srº(ª) não possui mais limite para compras.");
+            }else{
+                ExibeMensagem(@$"
+
+*************************************
+*                                   *
+*          Juros do Cartão          *
+*                                   *
+*  Pagamento em até 6X juros de 5%  *   
+*                                   *   
+*  Pagamento em até 7X ate 12X      *
+*  juros de 8%                      *
+*                                   *
+*************************************
+
+");
+            do{
+parcelas = PerguntaInt(@$"
+Em quantas vezes deseja pagar o valor de {valor} : 
+");
+            }while(parcelas<minParcelas && parcelas>maxParcelas);         
+            
+            if(parcelas<=6){
+                ExibeMensagemPulandoLinha(@$"O valor de {valor} parcelado em {parcelas}, da um total a pagar de {valor*1.05}");
+            }else{
+                ExibeMensagemPulandoLinha(@$"O valor de {valor} parcelado em {parcelas}, da um total a pagar de {valor*1.08}");
+            }
+            
+            }
+            
+        }
+
     }
 }
