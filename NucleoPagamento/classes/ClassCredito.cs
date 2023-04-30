a6845c24cb201c8731f31c19484651046fc443c8
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-04-30 07:30:14 -0300

    Feito validação dos dados e exibição na moeda corrente.

--- NucleoPagamento/classes/ClassCredito.cs
+++ NucleoPagamento/classes/ClassCredito.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace NucleoPagamento.classes
 {
     public class ClassCredito : ClassCartao
@@ -13,7 +15,7 @@ namespace NucleoPagamento.classes
             Console.WriteLine(pergunta);
             return char.Parse(Console.ReadLine());
         }
-       static int PerguntaInt(string pergunta)
+        static int PerguntaInt(string pergunta)
         {
             Console.WriteLine(pergunta);
             return int.Parse(Console.ReadLine());
@@ -36,53 +38,79 @@ namespace NucleoPagamento.classes
         }
 
         /*Atributo*/
-        private float Limite=6000;
+        private float Limite = 6000;
 
         public float valor = 200;
         /*Acessando Metodo da classe abstrata*/
-        public override void Pagar(){
-            
+        public override void Pagar()
+        {
+
             int parcelas;
             int minParcelas = 1;
             int maxParcelas = 12;
 
 
-            if(valor>Limite){
+            if (valor > Limite)
+            {
                 ExibeMensagemPulandoLinha("O Srº(ª) não possui mais limite para compras.");
-            }else{
+            }
+            else
+            {
                 ExibeMensagem(@$"
 
 *****************************************
 *                                       *
 *          Juros do Cartão              *
 *                                       *
-* 1)Pagamento em até 6X no cartao com   *    
+* - Pagamento em até 6X no cartao com   *    
 *   juros de 5%.                        *   
 *                                       *
-* 2) Pagamento em 7X ate 12X no cartao  *
+* - Pagamento em 7X ate 12X no cartao   *
 *    com juros de 8%                    *
 *                                       *
 *****************************************
 
 ");
-            do{
-parcelas = PerguntaInt(@$"
-Em quantas vezes deseja pagar o valor de {valor} : 
-"); 
-            if(parcelas<minParcelas||parcelas>maxParcelas){
-                Console.ForegroundColor = ConsoleColor.Red;
-                ExibeMensagemPulandoLinha("A ");
+
+                do
+                {
+                    /* parcelas = PerguntaInt(@$"
+                    Em quantas vezes deseja pagar o valor de {valor} : 
+                    "); */
+                    int.TryParse(PerguntaString(@$"
+Em quantas vezes deseja pagar o valor de {valor} :
+"), out parcelas);
+
+                    if (parcelas < minParcelas || parcelas > maxParcelas)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        ExibeMensagemPulandoLinha("\nA quantidade de parcelas digitadas é invalida, favor ditigar um número entre 1 e 12.");
+                        Console.ResetColor();
+                    }
+                } while (parcelas < minParcelas || parcelas > maxParcelas);
+
+                string vezes = "vezes";
+                if (parcelas == 1)
+                {
+                    vezes = "vez";
+                }
+
+                if (parcelas <= 6)
+                {
+
+                    ExibeMensagemPulandoLinha(@$"
+O valor de {(valor).ToString("C", new CultureInfo("pt-br"))} parcelado em {parcelas} {vezes}, da um total a pagar de {(valor * 1.05).ToString("C", new CultureInfo("pt-br"))}.
+");
+                }
+                else
+                {
+                    ExibeMensagemPulandoLinha(@$"
+O valor de {(valor).ToString("C", new CultureInfo("pt-br"))} parcelado em {parcelas} {vezes}, da um total a pagar de {(valor * 1.08).ToString("C", new CultureInfo("pt-br"))}.
+");
+                }
+
             }
-            }while(parcelas<minParcelas || parcelas>maxParcelas);         
-            
-            if(parcelas<=6){
-                ExibeMensagemPulandoLinha(@$"O valor de {valor} parcelado em {parcelas}, da um total a pagar de {valor*1.05}");
-            }else{
-                ExibeMensagemPulandoLinha(@$"O valor de {valor} parcelado em {parcelas}, da um total a pagar de {valor*1.08}");
-            }
-            
-            }
-            
+
         }
 
     }
