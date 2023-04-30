39c8c5cf0af5177b66e83ebeb5797a1cbd05a42b
kamille <kamille.senai@gmail.com>
2023-04-30 19:30:37 -0300

    .

--- NucleoPagamento/ClassPagamento.cs
+++ NucleoPagamento/ClassPagamento.cs
@@ -2,24 +2,27 @@ namespace NucleoPagamento
 {
     public class ClassPagamento
     {
-       
-       private var date = DateTime.Now;
+        private static DateTime Now;
 
-       public float valor;
+        public float valor;
 
+        public void Cancelar(string resposta)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Deseja Cancelar a operação? sim/não");
+            Console.ResetColor();
+            resposta = Console.ReadLine();
 
-        static void Cancelar(string resposta)
-       {
-         Console.WriteLine($"Deseja Cancelar a operação? sim/não");
-          resposta = Console.ReadLine();
+            if (resposta == "sim")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Operação cancelada");
+            }
 
-         if (resposta == "sim")
-         {
-            Console.WriteLine($"Cancelando");
+        
+
             
-         }
-          
-       }
+        }
 
 
     }
