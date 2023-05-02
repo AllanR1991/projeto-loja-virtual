c5c41fa2b8457d6022e92e62f5a6db08eeede37a
Evertonaraujo88 <evertonaraujosenai@gmail.com>
2023-05-02 10:38:29 -0300

    atualizacao ClassBoleto

--- NucleoPagamento/classes/ClassBoleto.cs
+++ NucleoPagamento/classes/ClassBoleto.cs
@@ -13,9 +13,35 @@ namespace NucleoPagamento.classes
          private  string CodigoDeBarra;
          
 
-        public float Valor = 1015f;
+        public float Valor = 205.50f;
 
         //Criação da função
+
+         public string Rnd( int n1,int n2)
+         {
+            Random Codigo = new Random();
+            int valorGerado = Codigo.Next( n1, n2);
+
+            if (valorGerado < 10)
+            {
+                return( "0000" + valorGerado);
+            }
+            else if (valorGerado < 100)
+            {
+                return ("000"+valorGerado);
+            }
+            else if (valorGerado < 1000)
+            {
+                return ("00" + valorGerado);
+
+            } else if (valorGerado < 10000)
+            {
+                return ("0" + valorGerado);
+            } 
+            return(""+valorGerado);
+            
+         }
+        
        
         public void Registrar()
     {
@@ -25,10 +51,11 @@ namespace NucleoPagamento.classes
         Console.WriteLine($" ");
            
         Random Codigo = new Random();
+        Codigo.Next( 0, 9);
         
         Console.WriteLine($"Código de barras do boleto: ");
         
-        CodigoDeBarra = $"{Codigo.Next(10000,50000)}.{Codigo.Next(10000,50000)} {Codigo.Next(10000,50000)}.{Codigo.Next(100000,500000)} {Codigo.Next(10000,50000)}.{Codigo.Next(100000,500000)} 9 {Codigo.Next(10000000,500000000)}{(ValorDesc).ToString("N2", CultureInfo.InvariantCulture).Replace(".","")}";
+        CodigoDeBarra = $"{Rnd(00001,50000)}.{Rnd(00001,50000)} {Rnd(00001,50000)}.{Rnd(100000,500000)} {Rnd(00001,50000)}.{Rnd(100000,500000)} {Codigo.Next( 0, 9)} {Rnd(10000000,500000000)}{(ValorDesc).ToString("N2", CultureInfo.InvariantCulture).Replace(".","")}";
         Console.WriteLine($"{CodigoDeBarra}");
         
        
